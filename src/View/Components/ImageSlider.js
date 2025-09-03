c9842cd56e3ede5d3bf81ecbeea96ea7b1343242
Yuanlim123 <yuanlim2000@hotmail.com>
2025-09-03 22:01:48 +0800

    wip image slider

---
+++ src/View/Components/ImageSlider.js
@@ -0,0 +1,35 @@
+import bgOne from "../../Assets/imgs/bg1.jpg";
+import bgTwo from "../../Assets/imgs/bg2.jpg";
+import bgThree from "../../Assets/imgs/bg3.jpg";
+
+function createImageSlider() {
+    const sliderDiv = document.createElement('div');
+    sliderDiv.classList.add('slider');
+    const slidesDiv = document.createElement('div');
+    slidesDiv.classList.add('slides');
+    
+    const imgs = [bgOne, bgTwo, bgThree];
+
+    imgs.forEach( (img, idx) => {
+        const imgEle = document.createElement('img');
+        imgEle.src = img;
+        imgEle.classList.add('slide');
+        imgEle.alt = `Img #${idx + 1}`;
+        slidesDiv.appendChild(imgEle);
+    });
+
+    const prevBtn = document.createElement('button');
+    prevBtn.classList.add('prev');
+    prevBtn.textContent = '<';
+    sliderDiv.appendChild(prevBtn);
+
+    const nextBtn = document.createElement('button');
+    nextBtn.classList.add('next');
+    nextBtn.textContent = '>';
+    sliderDiv.appendChild(nextBtn);
+
+    sliderDiv.appendChild(slidesDiv);
+    return sliderDiv;
+}
+
+export default createImageSlider;
--- src/View/Pages/Home/index.js
+++ src/View/Pages/Home/index.js
@@ -1,9 +1,15 @@
+import createImageSlider from "../../Components/ImageSlider";
+
 function createHomeIndex() {
     const containerDiv = document.createElement("div");
-
+    containerDiv.classList.add("home-index-container");
     const title = document.createElement("h2");
+    title.classList.add("title");
     title.textContent = "Welcome User";
     containerDiv.appendChild(title);
+
+    const imageSlider = createImageSlider();
+    containerDiv.appendChild(imageSlider);
 
     return containerDiv;
 }
